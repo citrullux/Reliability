8057f41c6eccfd40c19d76283a414e377143a8aa
Дмитрий Абдрахманов <565963@gmail.com>
2017-11-19 23:48:00 +0300

    Create Class

---
+++ Elements/Elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elements
+{
+    public class Link
+    {
+        public int LinkID;
+        public int ComponentStart;
+        public int ComponentEnd;
+    }
+
+    public class Unit
+    {
+        public int UnitID;
+    }
+
+    public class Component
+    {
+        public int ComponentID;
+    }
+}
